7ee484e20b4b5024549087015f8a0fbf2fd55a2a
henri-nog <fellipe.caluete@a.ficr.edu.br>
2025-11-24 12:05:15 -0300

    Modificando a pasta 'models' somente para teste

--- produtos/models.py
+++ produtos/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 class Produto(models.Model):
-    id_usuario = models.AutoField(primary_key=True) 
+    # id_usuario = models.AutoField(primary_key=True) 
+    # nome = models.CharField(max_length=100)
+    # email = models.EmailField()
+    # idade = models.IntegerField()
+    # curso = models.CharField(max_length=100)
+    # data_cadastro = models.DateTimeField(auto_now_add=True)
+
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
-    idade = models.IntegerField()
-    curso = models.CharField(max_length=100)
-    data_cadastro = models.DateTimeField(auto_now_add=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self) -> str:
        return self.nome
