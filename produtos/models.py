75889890a4714bc0e733c6f222d005c32a0bbc66
henri-nog <fellipe.caluete@a.ficr.edu.br>
2025-12-01 08:17:43 -0300

    Incluindo os campos necessários em 'models'

--- produtos/models.py
+++ produtos/models.py
@@ -1,11 +1,13 @@
-from django.db import models
+# produtos/models.py
 
+from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=70, blank=True)
-    idade = models.IntegerField()
-    curso = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    foto = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    disponibilidade = models.BooleanField(default=True)  # se o produto pode receber reservas
 
-    def __str__(self) -> str:
-       return self.nome
+    def __str__(self):
+        return self.nome
